3b7198b4981ca74f16a6d01f01a54073c7abbb30
Agnieszka Sierbista <agnieszka.sierbista@gmail.com>
2021-11-22 20:53:25 +0100

    Add examples of types of methods

---
+++ class_example_in_TS.ts
@@ -0,0 +1,26 @@
+class Animal {
+    attack = () => { //metoda instancji
+        this.run();
+    }
+
+    run() { //metoda prototypu
+        console.log("run!1");
+    }
+
+    run = () => { //metoda instancji
+        console.log("run!2");
+    }
+
+    static sleep() { // metoda klasy
+        this.findShelter();
+    }
+
+    static findShelter() {
+        console.log("finding shelter!")
+    }
+}
+
+const animal = new Animal();
+
+animal.attack();
+Animal.sleep();
