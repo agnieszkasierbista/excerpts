1d8fa3f8f629629b3e397fdc3ab2c9722b3dcfde
Agnieszka Sierbista <agnieszka.sierbista@gmail.com>
2021-12-03 09:38:53 +0100

    Tinker with parallel downloads

--- server/server.js
+++ server/server.js
@@ -5,13 +5,19 @@ const fs = require('fs');
 
 // app.use(express.static('public'))
 
-app.get('/public/stylesheet.css', (req, res) => {
+[1, 2, 3, 4, 5, 6, 7, 8].forEach((num) => {
+    app.get(`/public/stylesheet${num}.css`, (req, res) => {
 
-    setTimeout(() => {
-        res.header('Content-Type', 'text/css');
-        res.send(fs.readFileSync(__dirname + '/public/stylesheet.css'))
-    }, 2000);
+        console.log('request', req.url);
+
+        setTimeout(() => {
+            res.header('Content-Type', 'text/css');
+            res.send(fs.readFileSync(__dirname + `/public/stylesheet${num}.css`))
+        }, 12000);
+    })
 })
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
