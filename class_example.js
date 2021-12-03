ab122bb93182a83794589fc454a36c82f3524943
Agnieszka Sierbista <agnieszka.sierbista@gmail.com>
2021-12-03 10:17:28 +0100

    OOP basic rules

--- class_example.js
+++ class_example.js
@@ -1,39 +1,59 @@
-class Dog {
-    constructor(name) { this.name = name; } // this w konstruktorze odnosi sie do instancji
-    bark() { console.log('hau!', this.name); }
-    static run() { console.log('run!', this); }
+// class Dog {
+//     constructor(name) { this.name = name; } // this w konstruktorze odnosi sie do instancji
+//     bark() { console.log('hau!', this.name); }
+//     static run() { console.log('run!', this); }
+// }
+//
+// Dog.run();
+//
+// const a = new Dog("Fafik");
+//
+// a.bark();
+//
+// const b = new Dog("Azor");
+//
+// b.bark();
+//
+//
+// function Enemy(name) {
+//     this.name = name;
+// }
+//
+// Enemy.prototype = {
+//     attack: function() {
+//         console.log('attack!', this)
+//     },
+//     constructor: Enemy
+// }
+//
+// Enemy.hide = function hide() {
+//     console.log('hide!', this);
+// } // definiujemy metode statyczną (znajdującą sie bezpośrednio na klasie)
+//
+// Enemy.hide();
+//
+// var enemy = new Enemy("John");
+//
+// console.log('enemy', enemy);
+//
+// enemy.attack();
+
+class A {
+    greet() {
+        console.log('hi!');
+    }
 }
 
-Dog.run();
-
-const a = new Dog("Fafik");
-
-a.bark();
-
-const b = new Dog("Azor");
-
-b.bark();
-
-
-function Enemy(name) {
-    this.name = name;
+class B extends A { //dziedziczenie
+    run() {
+        console.log('run');
+    }
 }
 
-Enemy.prototype = {
-    attack: function() {
-        console.log('attack!', this)
-    },
-    constructor: Enemy
-}
+const a = new A();
+const b = new B();
 
-Enemy.hide = function hide() {
-    console.log('hide!', this);
-} // definiujemy metode statyczną (znajdującą sie bezpośrednio na klasie)
+a.greet();
+b.greet()
+b.run();
 
-Enemy.hide();
-
-var enemy = new Enemy("John");
-
-console.log('enemy', enemy);
-
-enemy.attack();
--- class_example_in_TS.ts
+++ class_example_in_TS.ts
@@ -23,5 +23,74 @@ class Animal {
 
 const animal = new Animal();
 
-animal.attack();
-Animal.sleep();
+// animal.attack();
+// Animal.sleep();
+
+class A {
+
+    private age = 20;
+    public name = 'sdfsdf;';
+
+
+    private greet() {
+        console.log('hi!');
+    }
+
+    public publicGreet() {
+        console.log(this.age);
+        this.greet();
+    }
+
+    protected szok() {
+        console.log('jajo');
+    }
+
+    public okejka() {
+        this.szok();
+    }
+}
+
+class B extends A {
+    myMethod() {
+        this.szok()
+        // this.greet();
+    }
+}
+
+const b = new B();
+const a = new A();
+
+a.publicGreet();
+b.publicGreet()
+
+b.myMethod()
+
+// a.szok();
+// b.szok();
+
+interface Shejp {
+    draw: () => null
+}
+
+abstract class Shape {
+    abstract draw(): void;
+}
+
+class Triangle extends Shape {
+    draw() {
+        console.log('triangle');
+    }
+}
+
+class Rectangle extends Shape {
+    draw() {
+        console.log('rectangle');
+    }
+}
+
+function szokio(a: Shape) {
+    a.draw();
+}
+
+szokio(new Rectangle());
+szokio(new Triangle());
